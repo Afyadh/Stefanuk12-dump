b42f715d183eb4acb6afa95ad10fd19e41095cc9
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-07 12:39:24 -0800

    Update SignalModule.lua

--- SignalModule.lua
+++ SignalModule.lua
@@ -1,71 +1,156 @@
--- // Dependencies
-local SignalBuilder = loadstring(game:HttpGet("https://raw.githubusercontent.com/Afyadh/Stefanuk12-dump/main/SignalModule.lua"))()
+-- // Vars
+local taskwait = task.wait or wait
 
--- // Manager Class
-local Manager = {}
-Manager.__index = Manager
-do
-    -- // Constructor
-    function Manager.new()
-        -- // Initialise class
-        local self = setmetatable({}, Manager)
+-- // Signal Class
+local Signal = {}
+Signal.__index = Signal
+Signal.__type = "Signal"
+Signal.ClassName = "Signal"
 
-        -- // Initialise vars
-        self.Signals = {}
+-- // Connection Class
+local Connection = {}
+Connection.__index = Connection
+Connection.__type = "Connection"
+Connection.ClassName = "Connection"
 
-        -- // Return class
-        return self
-    end
+-- // Connection Constructor
+function Connection.new(Signal: table, Callback: any): table
+    -- // Check Signal
+    local typeofSignal = typeof(Signal)
+    assert(typeofSignal == "table" and Signal.ClassName == "Signal", "bad argument #1 to 'new' (Signal expected, got " .. typeofSignal .. ")")
 
-    -- // Find a signal
-    function Manager.Get(self, SignalName)
-        return self.Signals[SignalName]
-    end
+    -- // Check callback
+    local typeofCallback = typeof(Callback)
+    assert(typeofCallback == "function", "bad argument #2 for 'new' (function expected, got " .. typeofCallback .. ")")
 
-    -- // Add a signal
-    function Manager.Add(self, Signal)
-        -- // Create signal if string is passed
-        if (typeof(Signal) == "string") then
-            Signal = SignalBuilder.new(Signal)
-        end
+    -- // Create
+    local self = setmetatable({}, Connection)
 
-        -- // Add
-        self.Signals[Signal.Name] = Signal
-    end
+    -- // Set properties
+    self.Function = Callback
+    self.State = true
+    self.Signal = Signal
 
-    -- // Remove a signal
-    function Manager.Remove(self, SignalName)
-        -- // Remove
-        self.Signals[SignalName] = nil
-    end
-
-    -- // Create a signal
-    Manager.Create = Manager.Add
-
-    -- // Fire a signal
-    function Manager.Fire(self, SignalName, ...)
-        -- // Get the signal
-        local Signal = self:Get(SignalName)
-
-        -- // Make sure signal exists
-        assert(Signal, "signal does not exist")
-
-        -- // Fire
-        return Signal:Fire(...)
-    end
-
-    -- // Connect to a signal
-    function Manager.Connect(self, SignalName, ...)
-        -- // Get the signal
-        local Signal = self:Get(SignalName)
-
-        -- // Make sure signal exists
-        assert(Signal, "signal does not exist")
-
-        -- // Connect
-        return Signal:Connect(...)
-    end
+    -- // Return new class
+    return self
 end
 
--- // Return
-return Manager
+-- // Enable a conneciton
+function Connection.Enable(self: table): nil
+    self.State = true
+end
+
+-- // Disable a connection
+function Connection.Disable(self: table): nil
+    self.State = false
+end
+
+-- // Disconnect a connection
+function Connection.Disconnect(self: table): nil
+    -- // Vars
+    local Connections = self.Signal.Connections
+    local selfInTable = table.find(Connections, self)
+
+    -- // Remove
+    table.remove(Connections, selfInTable)
+end
+Connection.disconnect = Connection.Disconnect
+
+-- // Signal Constructor
+function Signal.new(Name: string): table
+    -- // Check Name
+    local typeofName = typeof(Name)
+    assert(typeofName == "string", "bad argument #1 for 'new' (string expected, got " .. typeofName .. ")")
+
+    -- // Create
+    local self = setmetatable({}, Signal)
+
+    -- // Set properties
+    self.Name = Name
+    self.Connections = {}
+
+    -- // Return new class
+    return self
+end
+
+-- // Connect to a signal
+function Signal.Connect(self: table, Callback: any): table
+    -- // Check callback
+    local typeofCallback = typeof(Callback)
+    assert(typeofCallback == "function", "bad argument #1 for 'Connect' (function expected, got " .. typeofCallback .. ")")
+
+    -- // Create Connection Object
+    local connection = Connection.new(self, Callback)
+
+    -- // Add to connections
+    table.insert(self.Connections, connection)
+
+    -- // Return
+    return connection
+end
+Signal.connect = Signal.Connect
+
+-- // Fire a signal
+function Signal.Fire(self: table, ...): nil
+    -- // Loop through connections
+    for _, connection in ipairs(self.Connections) do
+        -- // See whether it can be fired
+        if not (connection.State) then
+            continue
+        end
+
+        -- // Fire
+        coroutine.wrap(connection.Function)(...)
+    end
+end
+Signal.fire = Signal.Fire
+
+-- // Wait for a signal
+function Signal.Wait(self: table, Timeout: number): any
+    -- // Default value
+    Timeout = Timeout or (1/0)
+    
+    -- // Vars
+    local returnVal = {}
+    local Fired = false
+
+    -- // Connect
+    local connection = self:Connect(function(...)
+        returnVal = {...}
+        Fired = true
+    end)
+
+    -- // Constant loop
+    local timeElapsed = tick()
+    while (true) do
+        -- // Wait
+        taskwait()
+
+        -- // Set time elapsed
+        timeElapsed = tick() - timeElapsed
+
+        -- // See if fired or timed out
+        if not (Fired or timeElapsed > Timeout) then
+            continue
+        end
+
+        -- // Break out of the loop
+        break
+    end
+
+    -- // Disconnect
+    connection:Disconnect()
+
+    -- // Return
+    return unpack(returnVal)
+end
+Signal.wait = Signal.Wait
+
+-- // Destroy a signal
+function Signal.Destroy(self: table): nil
+    self = nil
+end
+Signal.destroy = Signal.destroy
+
+-- //
+return Signal
